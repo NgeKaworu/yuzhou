51039821dcce26ff85504d79d18b26a1f50a3b90
fuRan <NgeKaworu@gmail.com>
2023-03-05 19:32:09 +0800

    perf: 更新umi4

--- workspace/time-mgt-umi/.umirc.ts
+++ workspace/time-mgt-umi/.umirc.ts
@@ -1,25 +1,39 @@
+/*
+ * @Author: fuRan NgeKaworu@gmail.com
+ * @Date: 2023-03-05 01:55:13
+ * @LastEditors: fuRan NgeKaworu@gmail.com
+ * @LastEditTime: 2023-03-05 19:07:36
+ * @FilePath: /monorepo-lab/workspace/time-mgt-umi/.umirc.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 import { defineConfig } from 'umi';
-import base from './src/js-sdk/configs/.umirc.default';
 import routes from './routes';
 import theme from './src/theme';
 
 export default defineConfig({
-  ...base,
+  plugins: ['@umijs/plugins/dist/qiankun'],
+  qiankun: {
+    slave: {},
+  },
+  hash: true,
+  runtimePublicPath: {},
+  npmClient: 'pnpm',
   theme,
   title: '柳比歇夫时间管理法',
   routes,
-  devServer: {
-    port: 8051,
-    proxy: {
-      '/api/time-mgt': {
-        target: 'http://localhost:8050',
-        changeOrigin: true,
-        pathRewrite: {
-          '/api/time-mgt': '',
-        },
+
+  proxy: {
+    '/api/time-mgt': {
+      target: 'http://localhost:8050',
+      changeOrigin: true,
+      pathRewrite: {
+        '/api/time-mgt': '',
       },
     },
   },
+
   base: '/time-mgt',
   publicPath: '/micro/time-mgt/',
 });
